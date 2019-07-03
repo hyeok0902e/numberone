5d8548776257a118ea733f6eb553494c0dfd718a
hyeoke <hyeoke@jeonjunhyeog-ui-MacBookPro.local>
2019-07-03 12:02:55 +0900

    hiring ~ing

--- routes/jobSearch/hiring.js
+++ routes/jobSearch/hiring.js
@@ -17,13 +17,14 @@ router.get('/', verifyToken, verifyDuplicateLogin, async (req, res, next) => {
     try {
         const user = await User.findOne({ where: { id: req.decoded.user_id } });
         const hirings = await Hiring.findAll({
-            where: { user_id: user.id },
+            where: { user_id: user.id, type: 0, },
             attributes: ['id', 'company', 'address', 'startDate', 'isSelected'],
             order: [['id', 'DESC']],   
             include: [
                 {
                     model: Labor,
                     order: [['id', 'ASC']], 
+                    limit: 1,
                 }
             ]
         });
@@ -42,26 +43,23 @@ router.get('/:hiring_id/show', verifyToken, verifyDuplicateLogin, async (req, re
         const { hiring_id } = req.params;
         if (!hiring_id) { response(res, 400, "params값 없음"); return; }
 
-        let hiring = await Hiring.findOne({
+        let hiring = await Hiring.findOne({ 
             where: { id: hiring_id },
             include: [
                 {
                     model: Labor,
-                    order: [['id', 'ASC']], 
+                    order: [['id', 'ASC']],
                 }
-            ]
+            ] 
         });
-        if (!hiring) { response(res, 404, "데이터 없음"); return; }
-
-        let payLoad = { hiring };
-        response(res, 200, "구인 상세페이지", payLoad);
+        if (!hiring) { response(res, 404, "구인정보가 존재하지 않습니다."); return; }
+        let payLoad = { hiring }
+        response(res, 200, "구인정보 상세 페이지", payLoad);
     } catch (err) {
-        console.log(err);ß
+        console.log(err);
         response(res, 500, "서버 에러");
     }
 });
-
-// 참여신청
 
 // 구인 등록
 router.post('/create', verifyToken, verifyDuplicateLogin, async (req, res, next) => {
@@ -73,9 +71,9 @@ router.post('/create', verifyToken, verifyDuplicateLogin, async (req, res, next)
 
         let a = hiring;
         let resHiring = await Hiring.create({
-            type: a.type, company: a.company, address: a.address, 
+            type: 0, company: a.company, address: a.address, 
             startDate: a.startDate, endDate: a.endDate, description: a.description,
-            isSelected: a.isSelected,
+            isSelected: 0,
         })
         await user.addHiring(resHiring);
         await asyncForEach(a.labor, async (l) => {
@@ -93,10 +91,10 @@ router.post('/create', verifyToken, verifyDuplicateLogin, async (req, res, next)
                     order: [['id', 'ASC']], 
                 }
             ]
-        })
+        });
         let payLoad = { hiring: resHiring };
         response(res, 201, "구인 등록 완료", payLoad);
-
+        
     } catch (err) {
         console.log(err);
         response(res, 500, "서버 에러");
@@ -109,19 +107,18 @@ router.get('/:hiring_id/edit', verifyToken, verifyDuplicateLogin, async (req, re
         const { hiring_id } = req.params;
         if (!hiring_id) { response(res, 400, "params값 없음"); return; }
 
-        let hiring = await Hiring.findOne({
+        let hiring = await Hiring.findOne({ 
             where: { id: hiring_id },
             include: [
                 {
                     model: Labor,
-                    order: [['id', 'ASC']], 
+                    order: [['id', 'ASC']],
                 }
-            ]
+            ] 
         });
-        if (!hiring) { response(res, 404, "데이터 없음"); return; }
-
-        let payLoad = { hiring };
-        response(res, 200, "구인 수정 페이지", payLoad);
+        if (!hiring) { response(res, 404, "구인정보가 존재하지 않습니다."); return; }
+        let payLoad = { hiring }
+        response(res, 200, "구인정보 수정 페이지", payLoad);
     } catch (err) {
         console.log(err);
         response(res, 500, "서버 에러");
@@ -132,17 +129,38 @@ router.get('/:hiring_id/edit', verifyToken, verifyDuplicateLogin, async (req, re
 router.put('/:hiring_id/edit', verifyToken, verifyDuplicateLogin, async (req, res, next) => {
     try {
         const { hiring_id } = req.params;
-        const { hiring } = req.body;
         if (!hiring_id) { response(res, 400, "params값 없음"); return; }
+        const { hiring } = req.body;
         if (!hiring) { response(res, 400, "데이터 없음"); return; }
-   
-        await Hiring.update(
-            { 
-                // 작성
-            },
-            { where: { id: hiring_id } },
-        )
 
+        let a = hiring;
+        await Hiring.update(
+            {
+                company: a.company, address: a.address, 
+                startDate: a.startDate, endDate: a.endDate, description: a.description,
+            },
+            { where: { id: hiring_id } }
+        );
+        await asyncForEach(a.labor, async (l) => {
+            await Labor.update(
+                {
+                    major: l.major, people: l.people,
+                },
+                { where: { hiring_id } }
+            );
+        });
+
+        let resHiring = await Hiring.findOne({
+            where: { id: hiring_id },
+            include: [
+                {
+                    model: Labor,
+                    order: [['id', 'ASC']], 
+                }
+            ]
+        });
+        let payLoad = { hiring: resHiring };
+        response(res, 200, "구인정보 수정 완료", payLoad);
     } catch (err) {
         console.log(err);
         response(res, 500, "서버 에러");
@@ -152,12 +170,42 @@ router.put('/:hiring_id/edit', verifyToken, verifyDuplicateLogin, async (req, re
 // 삭제
 router.delete('/:hiring_id/destroy', verifyToken, verifyDuplicateLogin, async (req, res, next) => {
     try {
-        const { hiring_id } = req.body;
-        // 데이터 없음
+        const { hiring_id } = req.params;
         if (!hiring_id) { response(res, 400, "params값 없음"); return; }
 
         await Hiring.destroy({ where: { id: hiring_id } });
-        response(res, 200, "삭제 완료");
+        response(res, 200, "구인정보 삭제 완료")
+    } catch (err) {
+        console.log(err);
+        response(res, 500, "서버 에러");
+    }
+});
+
+// 구인 참여신청
+router.post('/:hiring_id/participate', verifyToken, verifyDuplicateLogin, async (req, res, next) => {
+    try {
+        const { hiring } = req.body;
+        if (!hiring) { response(res, 400, "데이터 없음"); return; }
+
+        const user = await User.findOne({ where: { id: req.decoded.user_id } });
+
+        let a = hiring;
+        let resHiring = await Hiring.create({
+            type: 1, company: a.company, address: a.address, 
+            startDate: a.startDate, endDate: a.endDate, description: a.description,
+            isSelected: 0,
+        })
+        await user.addHiring(resHiring);
+        await asyncForEach(a.labor, async (l) => {
+            let labor = await Labor.create({
+                major: l.major, people: l.people,
+            });
+            await resHiring.addLabor(labor);
+        });
+
+        let payLoad = { hiring: resHiring };
+        response(res, 201, "구인 참여신청 완료 - 내 참여신청 목록 페이지로 이동", payLoad);
+        
     } catch (err) {
         console.log(err);
         response(res, 500, "서버 에러");
